757bc15b898ac828549fc991b9e61966deaf5a48
jakecimba <jakecimba@gmail.com>
2024-06-02 14:20:53 -0400

    Added recipe details screen and log out functionality

--- App.tsx
+++ App.tsx
@@ -7,6 +7,10 @@ import RecipesScreen from './src/components/RecipesScreen';
 import ProfileScreen from './src/components/ProfileScreen';
 import AddScreen from './src/components/AddScreen';
 export const Tab = createBottomTabNavigator();
+import {createStackNavigator} from '@react-navigation/stack';
+import RecipeDetailsScreen from './src/components/RecipeDetailsScreen';
+
+const Stack = createStackNavigator();
 
 const LightTheme = {
   ...DefaultTheme,
@@ -20,38 +24,60 @@ const AddScreenComponent = () => {
   return null;
 };
 
+function Home() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName = '';
+
+          if (route.name === 'Recipes') {
+            iconName = 'file-tray-full-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarStyle: {
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="Recipes" component={RecipesScreen} />
+      <Tab.Screen
+        name="Add"
+        component={AddScreenComponent}
+        options={{
+          tabBarButton: () => <AddScreen />,
+        }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export function App(): React.JSX.Element {
   return (
     <NavigationContainer theme={LightTheme}>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({color, size}) => {
-            let iconName = '';
-
-            if (route.name === 'Recipes') {
-              iconName = 'file-tray-full-outline';
-            } else if (route.name === 'Profile') {
-              iconName = 'person-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarStyle: {
-            borderTopWidth: 0,
-          },
-          tabBarActiveTintColor: 'black',
-          tabBarInactiveTintColor: 'gray',
-        })}>
-        <Tab.Screen name="Recipes" component={RecipesScreen} />
-        <Tab.Screen
-          name="Add"
-          component={AddScreenComponent}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false, headerTitle: ''}}
+        />
+        <Stack.Screen
+          name="RecipeDetailsScreen"
+          component={RecipeDetailsScreen}
           options={{
-            tabBarButton: () => <AddScreen />,
+            headerStyle: {
+              shadowColor: 'transparent',
+            },
+            headerTitle: '',
           }}
         />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
