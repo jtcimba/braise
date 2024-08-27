629a5f0b7338f02355e0bcceb3f8a388e77d6c9e
jakecimba <jakecimba@gmail.com>
2024-08-27 10:53:33 -0400

    Edit instructions with known bug

--- src/components/CloseIcon.tsx
+++ src/components/CloseIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function BackIcon(navigation: any) {
+function CloseIcon(navigation: any) {
   return (
     <TouchableOpacity onPress={() => navigation.goBack()}>
       <Ionicons name="close-circle" size={25} color="gray" />
@@ -10,4 +10,4 @@ function BackIcon(navigation: any) {
   );
 }
 
-export default BackIcon;
+export default CloseIcon;
