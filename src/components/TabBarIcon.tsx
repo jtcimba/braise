fe8f56818c072c20bab02d8e4f4e3d6227a965f6
jakecimba <jakecimba@gmail.com>
2025-12-21 14:14:34 -0500

    Added tab bar labels

--- src/components/TabBarIcon.tsx
+++ src/components/TabBarIcon.tsx
@@ -1,6 +1,13 @@
+import {useTheme} from '../../theme/ThemeProvider';
 import React, {useRef, forwardRef} from 'react';
-import {Animated, TouchableWithoutFeedback} from 'react-native';
+import {
+  Animated,
+  TouchableWithoutFeedback,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Theme} from '../../theme/types';
 
 interface TabBarIconProps {
   name: string;
@@ -10,13 +17,19 @@ interface TabBarIconProps {
 }
 
 const TabBarIcon = forwardRef<any, TabBarIconProps>(
-  ({name, color = 'gray', size = 24, onPressFunction}, ref) => {
+  ({name, color = 'gray', size = 22, onPressFunction}, ref) => {
     const scale = useRef(new Animated.Value(1)).current;
 
     const icons: {[key: string]: string} = {
       Recipes: 'file-tray-full-outline',
       'Grocery List': 'cart-outline',
       Add: 'add-circle-outline',
+    };
+
+    const labels: {[key: string]: string} = {
+      Recipes: 'Recipes',
+      'Grocery List': 'Shopping List',
+      Add: 'Add New',
     };
 
     const onPressIn = () => {
@@ -36,14 +49,31 @@ const TabBarIcon = forwardRef<any, TabBarIconProps>(
       onPressFunction();
     };
 
+    const theme = useTheme() as unknown as Theme;
+
     return (
       <TouchableWithoutFeedback onPressIn={onPressIn} onPressOut={onPressOut}>
-        <Animated.View ref={ref} style={{transform: [{scale}]}}>
+        <Animated.View
+          ref={ref}
+          style={[styles(theme).container, {transform: [{scale}]}]}>
           <Ionicons name={icons[name]} size={size} color={color} />
+          <Text style={[styles(theme).label, {color}]}>{labels[name]}</Text>
         </Animated.View>
       </TouchableWithoutFeedback>
     );
   },
 );
+
+const styles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 20,
+    },
+    label: {
+      ...theme.typography.h6,
+    },
+  });
 
 export default TabBarIcon;
